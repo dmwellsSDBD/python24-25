e2ecc9d36eefbc837317a8eba69fae665ff83ceb
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2024-11-14 13:40:32 +0000

    Thursday morning

---
+++ boolConditionalPractice.py
@@ -0,0 +1,70 @@
+print("\n\n\n")
+print("###########################################")
+print("##                                       ##")
+print("##              Practice 1               ##")
+print("##                                       ##")
+print("###########################################")
+print("\n\n\n")
+
+# Program to check if a number is positive, negative, or zero
+number = -3
+
+# Write an if/else (elif) structure to determine the number's positive, negative, zero state
+
+print("\n\n\n")
+print("###########################################")
+print("##                                       ##")
+print("##              Practice 2               ##")
+print("##                                       ##")
+print("###########################################")
+print("\n\n\n")
+
+# Program to check if a user has admin privileges
+user_role = "admin"
+
+# Write your Code Below
+
+
+
+print("\n\n\n")
+print("###########################################")
+print("##                                       ##")
+print("##              Practice 3               ##")
+print("##                                       ##")
+print("###########################################")
+print("\n\n\n")
+
+# Program to check if a year is a leap year
+year = 2024
+
+# Write your Code Below
+
+
+
+print("\n\n\n")
+print("###########################################")
+print("##                                       ##")
+print("##              Practice 4               ##")
+print("##                                       ##")
+print("###########################################")
+print("\n\n\n")
+
+# Program to classify age into age groups
+age = 25
+
+# Write your Code Below
+
+
+
+print("\n\n\n")
+print("###########################################")
+print("##                                       ##")
+print("##              Practice 5               ##")
+print("##                                       ##")
+print("###########################################")
+print("\n\n\n")
+
+# Program to determine discount based on purchase amount
+purchase_amount = 120
+
+# Write your code Below
