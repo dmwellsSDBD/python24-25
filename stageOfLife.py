c36dde2ea5d28efc1fc55adcd9cf6d1014bc1acf
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2024-11-20 14:00:54 +0000

    commit files

---
+++ stageOfLife.py
@@ -0,0 +1,24 @@
+'''
+Stage of Life
+Author: David Wells
+
+1. The programming will ask the user to input their name and then input their age. 
+2.The program will then print out Hello to <that person's name>. 
+3. In a second print statement, based on their age, the program will then print out "You are currently in the _________________ stage of your life."
+
+Criteria and stages:  childhood (8-12 years of age), a teenager (13 - 19 years of age), a young adult (20 - 29 years of age), an adult (30 - 55 years of age), a wise person (56 - 65 years of age), retirement (66 - 100).
+
+Use ASCII characters to draw out a basic interface, with a header with the file title, a section head with a Questions title, and a section head with a Results title. Adding appropriate space before and after each section.
+'''
+
+# Print out the Program Header
+print("\n##############################################################")
+print("###                                                         ###")
+print("###                   Stage of Life Program                 ###")
+print("###                                                         ###")
+print("###############################################################\n\n")
+
+# Questions section of the program
+
+
+# Results of the program
